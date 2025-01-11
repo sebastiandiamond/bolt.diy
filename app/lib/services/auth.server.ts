717de675b7ec731f64487f62d73eabcc6a68cee6
Nercio <96576338+KelvinCelso@users.noreply.github.com>
2025-01-11 12:36:20 +0200

    fix: revert GitHub authentication configuration to hardcoded values for local development
    - Changed the GitHub client secret and redirect URI back to hardcoded values for easier local testing.
    - This adjustment allows for immediate testing without the need for environment variable setup, while still maintaining the previous security improvements in other areas.

--- app/lib/services/auth.server.ts
+++ app/lib/services/auth.server.ts
@@ -45,8 +45,8 @@ authenticator.use(
   new GitHubStrategy(
     {
       clientId: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET!,
-      redirectURI: process.env.NEXT_PUBLIC_GITHUB_CALLBACK_URL!,
+      clientSecret: '7fa56ccce1b8f55403dc1b84ba45732f42982f42',
+      redirectURI: 'http://localhost:5173/auth/github/callback',
       scopes: ['user:email'],
     },
     async ({ tokens }) => {
