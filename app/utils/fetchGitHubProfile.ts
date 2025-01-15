9bd9341b1046500b4edc7b8e795f7d186f827791
kelvincelso <96576338+KelvinCelso@users.noreply.github.com>
2025-01-15 22:08:09 +0200

    test token

--- app/utils/fetchGitHubProfile.ts
+++ app/utils/fetchGitHubProfile.ts
@@ -5,7 +5,7 @@ export async function fetchGitHubProfile(accessToken: string) {
   const response = await fetch('https://api.github.com/user', {
     headers: {
       Accept: 'application/vnd.github+json',
-      Authorization: `Bearer ${accessToken}`,
+      Authorization: `Bearer ghu_RKUvAgOcIhkgRsGtKsJDxrjnpnJFNk1cLTbq`,
       'X-GitHub-Api-Version': '2022-11-28',
     },
   })
