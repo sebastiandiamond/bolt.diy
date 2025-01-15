6855a847437af72b16e1b9e876ad7856c55e32fc
kelvincelso <96576338+KelvinCelso@users.noreply.github.com>
2025-01-15 21:52:48 +0200

    debuggin prod

--- app/utils/fetchGitHubProfile.ts
+++ app/utils/fetchGitHubProfile.ts
@@ -1,16 +1,16 @@
 import { logger } from "./logger";
 
 export async function fetchGitHubProfile(accessToken: string) {
+  logger.info(`Fetching GitHub profile with access token: ${accessToken}`);
   const response = await fetch('https://api.github.com/user', {
     headers: {
       Accept: 'application/vnd.github+json',
       Authorization: `Bearer ${accessToken}`,
       'X-GitHub-Api-Version': '2022-11-28',
     },
-  });
-  const data = await response.json();
+  })
 
-  logger.info(`GitHub profile response: ${data}`);
+
   logger.info(`GitHub profile response status: ${response.status}`);
   logger.info(`GitHub profile response status text: ${response.ok}`);
   if (response.status !== 200) {
